2cede4a112589a3ac59cb4b1c1752f09e89fa6b2
Camya Robinson <camyacodes@gmail.com>
2024-08-12 00:46:32 -0400

    added flexbox layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css'
+import cats from '../cat_data.json'
 import CatCard from './components/CatCard'
 
 function App() {
-  return <CatCard />
+  return (
+    <div className='container'>
+      {cats.map((cat, index) => (
+        <CatCard key={index} cat={cat} />
+      ))}
+    </div>
+  )
 }
 
 export default App
--- src/components/CatCard.tsx
+++ src/components/CatCard.tsx
@@ -1,22 +1,34 @@
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 
-const CatCard = () => {
+interface Cat {
+  Owner: string
+  cat_name: string
+  breed: string
+  image: string
+  description: string
+}
+
+interface CatCardProps {
+  cat: Cat
+}
+
+const CatCard = ({ cat }: CatCardProps) => {
+  const { Owner, cat_name, image } = cat
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img
-        variant='top'
-        width='200'
-        height='200'
-        src='https://www.purina.com/_gatsby/image/fd6bdb5eb57d6bc842746800cf20f582/3f561170167ef1bfda649098ed0314c7/Abyssinian_body_7.avif?u=https%3A%2F%2Fwww.purina.com%2Fsites%2Fdefault%2Ffiles%2Fmedia%2Fimage%2FAbyssinian_body_7.jpg&a=w%3D500%26h%3D500%26fm%3Davif%26q%3D75&cd=ecb68feab94eba241d13f805758a0ad5'
-      />
+      <Card.Img variant='top' width='200' height='200' src={image} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>
+          {cat_name} <br />
+          <em>{Owner}</em>
+        </Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {/* <p>Owner: {Owner}</p> */}
+          {/* <p>Breed: {breed}</p> */}
+          {/* <p>{description}</p> */}
         </Card.Text>
-        <Button variant='primary'>Go somewhere</Button>
+        {/* <Button variant='primary'>Go somewhere</Button> */}
       </Card.Body>
     </Card>
   )
